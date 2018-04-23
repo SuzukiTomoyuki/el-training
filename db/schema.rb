f750feb568f4eac563b6c328f1a58fa3fd049519
SuzukiTomoyuki <t.suzuki@ashita-team.com>
2018-04-23 15:50:12 +0900

    taskテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423012036) do
+ActiveRecord::Schema.define(version: 20180423051844) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "priority"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180423012036) do
     t.string "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
