664dde493620e011d40aa878f0d118733752d912
SuzukiTomoyuki <t.suzuki@ashita-team.com>
2018-04-25 14:32:28 +0900

    削除ができる

--- app/controllers/tasks_list_controller.rb
+++ app/controllers/tasks_list_controller.rb
@@ -5,44 +5,34 @@ class TasksListController < ApplicationController
 
   def new
     @task = Task.new
-    render action: :new
-    # @task.update_time = Date.today
-    # @task.save
-    # render action: :new
-    # respond_to do |format|
-    #   if @task.save
-    #     format.html { redirect_to @task, notice: 'task was successfully created.' }
-    #     format.json { render :show, status: :created, location: @task }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @task.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   def create
-    # Task.create(create_params)
     @task = Task.new(create_params)
     if @task.save
       redirect_to tasks_list_index_path
     else
       render new_tasks_list_path
     end
-    # render action: :new
-    # respond_to do |format|
-    #   if @task.save
-    #     format.html { redirect_to @task, notice: 'task was successfully created.' }
-    #     format.json { render :show, status: :created, location: @task }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @task.errors, status: :unprocessable_entity }
-    #   end
-    # end
+  end
+
+  def update
+    @task = find_task_by_id
+  end
+
+  def destroy
+    @task = find_task_by_id
+    @task.destroy
+    redirect_to tasks_list_index_path
   end
 
   private
   def create_params
-    params.require(:task).permit(:caption, :priority, :deadline, :state, :label)
+    params.require(:task).permit(:id, :caption, :priority, :deadline, :state, :label)
+  end
+
+  def find_task_by_id
+    Task.find(params[:id])
   end
 
 end
