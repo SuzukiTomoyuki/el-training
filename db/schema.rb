ad05b0318fafadbe83f0705dc52ffc570637cdea
SuzukiTomoyuki <t.suzuki@ashita-team.com>
2018-05-10 10:57:30 +0900

    indexをtaskに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509083704) do
+ActiveRecord::Schema.define(version: 20180510013642) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "priority_id", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180509083704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
