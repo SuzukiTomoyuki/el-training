80971d0e0f43fc77db81a5af0bd8da3a88d94704
SuzukiTomoyuki <t.suzuki@ashita-team.com>
2018-05-17 14:59:24 +0900

    update migrate

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # post '/new' => 'tasks#new'
   # get '/tasks/:id' => 'tasks#update'
   # post '/tasks/:id' => 'tasks#update'
-  resources :tasks
+  resources :tasks, :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
