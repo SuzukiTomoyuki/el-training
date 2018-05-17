b292dc145eb0cf0c42ea3c26e87184652abf5e88
SuzukiTomoyuki <t.suzuki@ashita-team.com>
2018-05-17 15:22:45 +0900

    add migrate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(create_params)
+  end
+
+  private
+  def create_params
+    params.require(:user).permit(:id, :name, :pass)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517055146) do
+ActiveRecord::Schema.define(version: 20180517062028) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "deadline"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180517055146) do
     t.string "pass"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
