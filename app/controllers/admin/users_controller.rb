45812290f11aa014904139460b5ebabcfb7c10d4
SuzukiTomoyuki <t.suzuki@ashita-team.com>
2018-05-28 18:20:38 +0900

    status初期値修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,6 +9,7 @@ class Admin::UsersController < ApplicationController
 
   def destroy
     @user = find_user_by_id
+    destroy_task
     @user.destroy
     flash[:notice] = "ユーザとタスク削除完了"
     redirect_to admin_users_path
@@ -40,5 +41,10 @@ class Admin::UsersController < ApplicationController
 
   def find_user_by_id
     User.find(params[:id])
+  end
+
+  def destroy_task
+    task = Task.find_by_user_id(find_user_by_id)
+    task.destroy
   end
 end
