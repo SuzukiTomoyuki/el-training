7b7332770f3b124e9d271dd5ed3bc412c20e69f1
SuzukiTomoyuki <t.suzuki@ashita-team.com>
2018-06-20 07:45:47 +0000

    capistoranoの設定（途中）

---
+++ config/deploy.rb
@@ -0,0 +1,53 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "el-training"
+set :repo_url, "git@github.com:SuzukiTomoyuki/el-training.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'master'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/www/rails/el-training"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "bundle", "public/assets"
+
+# Default value for default_env is {}
+set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+# set :scm, :git
+set :pry, true
+
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'bundle exec pumactl -F config/puma.rb stop'
+    invoke 'bundle exec pumactl -F config/puma.rb start'
+  end
+  after :finishing, 'deploy:cleanup'
+end
+
