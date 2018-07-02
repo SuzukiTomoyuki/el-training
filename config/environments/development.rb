a8127dd9dad2047e2925a5583c68acbda7c9837f
SuzukiTomoyuki <t.suzuki@ashita-team.com>
2018-07-02 18:37:55 +0900

    fix development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # 画像をメーラーに載せる
-  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_controller.asset_host = 'http://watashino-yorokobi-tanoshiina.herokuapp.com/'
   config.action_mailer.asset_host = config.action_controller.asset_host
   config.action_mailer.default_url_options= { host:'localhost:3000' }
 
